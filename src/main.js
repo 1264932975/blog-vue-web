d227bd2eb9cad9d7378c45a5069589b8993f0aba
shield <1264932975@qq.com>
2022-11-30 19:16:58 +0800

    seo和专题

--- src/main.js
+++ src/main.js
@@ -7,11 +7,9 @@ import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import './style.css'
 import ItemList from './components/ItemList.vue'
 import RowCard from './components/RowCard.vue'
-import {changeTitle} from "@/util/ChangeTitle";
 
 const app = createApp(App)
 
-app.use(changeTitle)
 app.use(router)
 app.use(ElementPlus, {
     locale: zhCn,
