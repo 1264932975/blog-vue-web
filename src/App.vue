5bebe9b07d8a67536e8b38ce4ed93b7e87634f3c
shield <1264932975@qq.com>
2022-12-04 14:55:48 +0800

    页面布局调整

--- src/App.vue
+++ src/App.vue
@@ -47,17 +47,13 @@
       </el-main>
       <el-footer class="footer">
         <template v-if="webData.recordState" class="footer">
-          <el-col>
             <a href="https://beian.miit.gov.cn/" target="_blank">{{ webData.icpNo }}</a>
-          </el-col>
-          <el-col>
             <a target="_blank"
                href="http://www.beian.gov.cn/portal/registerSystemInfo"
-               style="display:inline-block;text-decoration:none;"><img
+               style="display:inline-block;text-decoration:none; padding-left: 20px"><img
                 src="https://www.beian.gov.cn/img/new/gongan.png" style="float:left;"/>
               <p style="float:left;margin: 0px 0px 0px 5px; color:#939393;">
                 {{ webData.policeNo }}</p></a>
-          </el-col>
         </template>
       </el-footer>
     </el-container>
